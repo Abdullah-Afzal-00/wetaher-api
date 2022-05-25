434d1377feeb5a7e9558c7c3a9ff1c334f53d97e
Abdullah-Afzal-00 <mabdullahafzal09@gmail.com>
2022-05-25 12:14:13 +0500

    Wether API

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+const App = () => {
+  const APIkey = "ee32bc1e5006043421553617e392b255";
+  const [data, setData] = useState("");
+  const [cityName, setCityName] = useState("");
+  const [temprature, setTemprature] = useState("");
+  const [info, setInfo] = useState("");
+  const getCityName = () => {
+    console.log(data);
+    setCityName(data);
+    setInfo(" has temprature ");
+  };
+  const getTemprature = () => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIkey}&units=metric`
+      )
+      .then((res) => {
+        console.log(res);
+        setTemprature(res.data.main.temp);
+        //console.log(temprature);
+      })
+      .catch((e) => {
+        console.log("Error is Found !!!");
+      });
+  };
+  return (
+    <>
+      <input
+        placeholder="Enter City Name"
+        onChange={(event) => setData(event.target.value)}
+      ></input>
+      <button
+        onClick={() => {
+          getCityName();
+          getTemprature();
+        }}
+      >
+        Show Weather
+      </button>
+      <div>
+        {cityName}
+        {info}
+        {temprature}
+      </div>
+    </>
+  );
+};
+
+export default App;
